3ae62b91e61a0d9487fba77698bbbb01fae4a42e
rtsmoor <57296077+rtsmoor@users.noreply.github.com>
2020-01-06 15:23:41 +0100

    code afgrond detectie
    basiscode voor detecteren of de afstand kleiner is dan 5 cm (max afstand is 30cm)
    sensor nog NIET gekalibreerd.

---
+++ afgrondDetectie.ino
@@ -0,0 +1,52 @@
+const int trigPin = 19;
+const int echoPin = 18;
+
+long duration;
+int distance;
+
+
+void setup() {
+pinMode(trigPin, OUTPUT);
+pinMode(echoPin, INPUT);
+Serial.begin(9600);
+}
+
+void loop() {
+  
+int afgrond = afgrondDetect();
+//Serial.println(afgrond);
+}
+
+int afgrondDetect(){
+  
+  // Clears the trigPin
+digitalWrite(trigPin, LOW);
+delayMicroseconds(2);
+
+// Sets the trigPin on HIGH state for 10 micro seconds
+
+digitalWrite(trigPin, HIGH);
+delayMicroseconds(10);
+digitalWrite(trigPin, LOW);
+
+// Reads the echoPin, returns the sound wave travel time in microseconds
+duration = pulseIn(echoPin, HIGH);
+
+// Calculating the distance
+distance = duration*0.034/2;
+
+if(distance > 30){
+  distance = 30;  
+}
+// Prints the distance on the Serial Monitor
+Serial.print("Distance: ");
+Serial.print(distance);
+Serial.println(" cm");
+if(distance > 5) {
+  return 1;
+}
+if (distance <= 5) {
+  return 0;
+  }
+
+  }
