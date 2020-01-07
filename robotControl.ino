6a89829eb9986359edcb60a1065664ae3a88ba15
rtsmoor <57296077+rtsmoor@users.noreply.github.com>
2020-01-07 13:46:39 +0100

    rij-functies toegevoegd
    stopt nog niet bij afgrond

--- robotControl.ino
+++ robotControl.ino
@@ -47,18 +47,18 @@ String detectie = "NO";
 String obstacle = "NO";
 
 
-  // afgrond detectie
-  const int trigPinA = 19;
-  const int echoPinA = 18;
+// afgrond detectie
+const int trigPinA = 19;
+const int echoPinA = 18;
 
 
 void setup() {
   //
   Serial.begin(115200);
-// afgrond detectie
+  // afgrond detectie
   pinMode(trigPinA, OUTPUT);
   pinMode(echoPinA, INPUT);
-  
+
   // Initialize the output variables as outputs
   pinMode(motorLinksVooruit, OUTPUT);
   pinMode(motorRechtsVooruit, OUTPUT);
@@ -98,31 +98,31 @@ void setup() {
 }
 
 void loop() {
-  int waardeInfraroodSensorLinks = 1- digitalRead(infraroodSensorLinks);
+  int waardeInfraroodSensorLinks = 1 - digitalRead(infraroodSensorLinks);
 
   static unsigned long startTime = millis();
-//---------------------------------- nieuwe code hier----------------------------
+  //---------------------------------- nieuwe code hier----------------------------
   //Serial.println(WiFi.localIP());
   if (modus == "Automatic-driving") {
     Serial.println("Automatic-driving");
 
     bool afgrond = afgrondDetectie();
-    if(afgrond == true){
+    if (afgrond == true) {
       //robotStop();
       //robotReverse();
       //robotLeft or robotRight
       //robotForward
-      }
-     
+    }
 
-    
+
+
     if (reedSensor == HIGH) {
       counter++;
       detectie = "YES";
     }
     if (reedSensor == LOW) {
       detectie = "NO";
-    }    
+    }
 
     if (millis () - startTime > 500) {
       startTime = millis();
@@ -147,7 +147,7 @@ void loop() {
           }
     */
   }
-// -----------------------------nieuwe code tot hier----------------------
+  // -----------------------------nieuwe code tot hier----------------------
   WiFiClient client = server.available();   // Listen for incoming clients
 
   if (client) {                             // If a new client connects,
@@ -423,4 +423,34 @@ bool afgrondDetectie() {
     return false;
   }
 
+}
+
+void robotForward() {
+  robotStop();
+  digitalWrite(motorLinksVooruit, HIGH);
+  digitalWrite(motorRechtsVooruit, HIGH);
+}
+
+void robotLeft() {
+  digitalWrite(motorLinksVooruit, LOW);
+  digitalWrite(motorRechtsVooruit, HIGH);
+}
+
+void robotRight() {
+  digitalWrite(motorLinksVooruit, HIGH);
+  digitalWrite(motorRechtsVooruit, LOW);
+}
+
+void robotReverse() {
+  robotStop();
+  digitalWrite(motorLinksAchteruit, HIGH);
+  digitalWrite(motorRechtsAchteruit, HIGH);
+}
+
+void robotStop() {
+  digitalWrite(motorLinksAchteruit, LOW);
+  digitalWrite(motorRechtsAchteruit, LOW);
+  digitalWrite(motorLinksVooruit, LOW);
+  digitalWrite(motorRechtsVooruit, LOW);
+  
 }
